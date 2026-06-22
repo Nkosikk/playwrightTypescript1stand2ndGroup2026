84a7d6df026e53cc689f91462a6b2ba19f9c6cbb
Mahlatse03 <mahlatsed@gmail.com>
2026-06-22 18:35:05 +0200

    Added API test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,9 @@ import {expect, test} from '../src/fixtures/CustomFixtures';
 import {validUsers, invalidUsers} from '../src/data/Testdata';
 
 
-test('Positive login - Admin', async ({ loginPage, homePage, page }) => {
+test.describe ('Login functionality', () => {
+
+    test('Positive login - Admin', async ({ loginPage, homePage, page }) => {
     await loginPage.basePageGoToUrl('/');
     await loginPage.navigateToLoginPage();
     await loginPage.userLogin(validUsers.admin.username, validUsers.admin.password);
@@ -13,3 +15,18 @@ test('Positive login - Admin', async ({ loginPage, homePage, page }) => {
     await homePage.verifyHomePageIsDisplayed();
 });
 
+    test.only('Positive login via API - class user', async ({request}) => {
+        const response = await request.post('https://www.ndosiautomation.co.za/APIDEV/login', {
+        //payload
+        data: {
+            "email": validUsers.classUser.username,
+            "password": validUsers.classUser.password
+            }
+        });
+        const body = await response.json();
+        console.log (body);
+        expect(response.status()).toBe(200); 
+
+    });
+
+});
